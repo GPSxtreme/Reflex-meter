d6a4b9c6497354c91522240a61434a50f34e7041
royal-lobster <srujangs8@gmail.com>
2022-02-11 17:08:15 +0530

    final commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="headCntnr">
+    <h1>{{ head }}</h1>
+    <button class="btn" @click="start" :disabled="isPlaying">Play</button>
+    <p v-if="showResult">
+      {{ `Your reaction time is ${this.reactionTime}ms!` }}
+    </p>
+    <p v-if="showResult">{{ `#${complimentReturner()}` }}</p>
+    <p v-if="showResult">Wanna try again? Press on play</p>
+  </div>
+  <block v-if="isPlaying" :delay="delay" @end="endGame" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+//import other vue files
+import Block from "./components/Block.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      head: "Test your reflexes!",
+      isPlaying: false,
+      delay: null,
+      showResult: false,
+      reactionTime: null,
+    };
+  },
+  components: { Block },
+  methods: {
+    start() {
+      this.delay = 1000 + Math.random() * 3000;
+      this.isPlaying = true;
+      this.showResult = false;
+    },
+    endGame(status) {
+      this.isPlaying = false;
+      this.showResult = true;
+      this.reactionTime = status;
+    },
+    complimentReturner() {
+      if (this.reactionTime <= 180) return "Ninja speed!";
+      else if (this.reactionTime <= 280) return "Quick reflexes!";
+      else if (this.reactionTime <= 380) return "Meh.. Can be better";
+      else if (this.reactionTime <= 580) return "Got any eye problems?";
+      else return "Boomer";
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+h1 {
+  font-size: 20 px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #000;
+}
+.headCntnr {
+  margin-top: 120px;
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+  flex-direction: column;
+  align-items: center;
+}
+p {
+  text-align: center;
+  font-weight: 700;
+  font-size: 20px;
 }
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="gameBdy">
+    <div class="block" v-if="showBlock" @click="stopTimer">
+      <span class="clickMe">click me!</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer();
+    }, this.delay);
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      // console.log(this.reactionTime);
+      this.$emit("end", this.reactionTime);
+    },
+  },
+};
+</script>
+
+<style>
+.block {
+  /* only styles */
+  background-color: rgb(59, 245, 68);
+  color: rgb(63, 62, 62);
+  border-radius: 16px;
+  max-width: 15%;
+  max-height: 15%;
+  padding: 50px 50px;
+  margin: 40px auto;
+  text-align: center;
+  font-size: 20px;
+  font-weight: 700;
+  box-shadow: 2.8px 2.8px 2.2px rgba(0, 0, 0, 0.014),
+    6.7px 6.7px 5.3px rgba(0, 0, 0, 0.02),
+    12.5px 12.5px 10px rgba(0, 0, 0, 0.025),
+    22.3px 22.3px 17.9px rgba(0, 0, 0, 0.03),
+    41.8px 41.8px 33.4px rgba(0, 0, 0, 0.036),
+    100px 100px 80px rgba(0, 0, 0, 0.05);
+}
+clickMe:hover {
+  font-size: 30px;
+  cursor: default;
+}
+.gameBdy {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  gap: 20px;
+}
+</style>
